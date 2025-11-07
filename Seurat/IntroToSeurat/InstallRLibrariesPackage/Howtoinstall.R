b2a70c1d3bc99fea7075c85010e2b145888ad2c9
AlineRngl <aline.rangelolguin@gmail.com>
2025-11-07 12:43:41 -0500

    Create Howtoinstall.R

---
+++ Seurat/IntroToSeurat/InstallRLibrariesPackage/Howtoinstall.R
@@ -0,0 +1,4 @@
+install.packages("devtools") # Install devtools
+devtools::install("path/to/InstallLibraries") # intall package to load libraries
+library(InstallLibraries_IBRO)
+load_my_libs() #Check if my libraries are installed, if not then install them
